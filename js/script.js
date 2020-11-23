38f1bd172f94585c802daeabcbade21ac053eb7c
rafael oliveira <rafael.oliveira@helps-on.com>
2020-11-23 17:39:54 -0300

    tooltip

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimationScroll from './modules/scroll-animation.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tab-nav.js';
 import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
 
 initSmoothScroll();
 initAnimationScroll();
 initAccordion();
 initTabNav();
 initModal();
+initTooltip();
