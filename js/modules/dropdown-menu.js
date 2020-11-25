cab37e0880dfc85e1dd1881ca846862b9ed53922
rafael oliveira <rafael.oliveira@helps-on.com>
2020-11-25 19:22:31 -0300

    outsideClick

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,9 +1,14 @@
+import outsideClick from './outsideclick.js';
+
 export default function initDropdownMenu() {
   const dropdownMenus = document.querySelectorAll('[data-dropdown]');
 
   function handleClick(event) {
     event.preventDefault();
-    this.classList.toggle('active');
+    this.classList.add('active');
+    outsideClick(this, ['touchstart', 'click'], () => {
+      this.classList.remove('active');
+    });
   }
 
   dropdownMenus.forEach((menu) => {
