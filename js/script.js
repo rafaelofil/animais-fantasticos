60668b3a3e9c0323677b97bd2ed64d9980072779
rafael oliveira <rafael.oliveira@helps-on.com>
2021-01-05 16:42:25 -0300

    Refactor Tooltips

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import initAnimationScroll from './modules/scroll-animation.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tab-nav.js';
 import Modal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
+import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initAnimateNumbers from './modules/animate-numbers.js';
@@ -27,8 +27,10 @@ const modal = new Modal(
 );
 modal.init();
 
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
+
 initAnimationScroll();
-initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initAnimateNumbers();
